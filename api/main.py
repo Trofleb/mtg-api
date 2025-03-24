b531c050ae95ff273e4994cb9e52830fb04c6250
Nicolas Casademont <nico@ketl.ch>
2025-03-24 23:06:04 +0100

    fix: Add ocr to the app

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from api.router import base, cards, dnd_rules, mtg_rules, sets
+from api.router import base, cards, dnd_rules, mtg_rules, ocr, sets
 
 app = FastAPI()
 
@@ -9,3 +9,4 @@ app.include_router(sets.router)
 app.include_router(cards.router)
 app.include_router(dnd_rules.router)
 app.include_router(mtg_rules.router)
+app.include_router(ocr.router)
